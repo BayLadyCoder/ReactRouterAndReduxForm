536098aaa124d8abfe012f58237309f99be413fa
Rachada Bay Chairangsaris <rachada_32@hotmail.com>
2018-05-25 14:32:23 -0400

    Created renderTitleField function to render some JSX input form and connected with {...field.input}

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,12 +2,25 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+
+  renderTitleField(field) {
+    return (
+      <div className="form-group">
+      <label>Title</label>
+        <input className="form-control"
+        type="text"
+          {...field.input}
+        />
+      </div>
+    );
+  }
+
   render() {
     return (
       <form>
         <Field
           name="title"
-          component={}
+          component={this.renderTitleField}
         />
       </form>
     );
