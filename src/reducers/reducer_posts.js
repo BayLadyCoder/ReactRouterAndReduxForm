78942f3f4c5f437d2654dfd0950c8d2c37a6b8bb
Rachada Bay Chairangsaris <rachada_32@hotmail.com>
2018-05-30 15:32:19 -0400

    Added fetchPost function in the ActionCreator. and added it to a new switch case in ReducerPosts returning an object with key = id and value = post data, so we can access to the data using post id.

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,17 @@
 import _ from 'lodash';
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS, FETCH_POST } from '../actions';
 
 export default function (state = {}, action) {
   switch (action.type) {
+    case FETCH_POST:
+      // const post = action.payload.data;
+      // const newState = { ...state,  };
+      // newState[post.id] = post;
+      // return newState;
+
+      return { ...state, [action.payload.data.id]: action.payload.data };
+      // [action.payload.data.id] is not creating an array
+      // it's making a new key in this object, key = id and value is the post data
     case FETCH_POSTS:
       // return console.log(action.payload.data); // [post1, post2]
       return _.mapKeys(action.payload.data, 'id');
