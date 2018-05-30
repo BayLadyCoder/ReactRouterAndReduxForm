11fa307d0ac9792e0c78fc3b0b625c4bad687b60
Rachada Bay Chairangsaris <rachada_32@hotmail.com>
2018-05-30 15:57:41 -0400

    import fetchPost in PostsShow component and added mapStateToProps and use ownProps to pass only the active post data to the component instead of passing all the posts from the application state. Defined componentDidMount function to do fetchPost once the component didplays.

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
+  componentDidMount() {
+    const { id } = this.props.match.params; // get access to  the url route with :id
+    // same as const id = this.props.match.params.id;
+
+    this.props.fetchPost(id);
+  }
+
   render() {
     return (
       <div>
@@ -10,4 +19,14 @@ class PostsShow extends Component {
   };
 }
 
-export default PostsShow;
+// in mapStateToProps, the first argument must be the application newState
+// and the second argument is referred to by convention as ownProps
+// ownProps is the props object that is going to specifically this component(in this case is PostsShow)
+// ownProps is like this.props
+function mapStateToProps({ posts }, ownProps) {
+  return { posts: posts[ownProps.match.params.id] };
+
+}
+
+
+export default connect(null, { fetchPost })(PostsShow);
