cefa376a459246f902d46cedbc9bddb58896c53c
Rachada Bay Chairangsaris <rachada_32@hotmail.com>
2018-05-31 13:20:15 -0400

    Added a delete button in PostsShow, added DELETE_POST ActionCreator, and use _.omit in ReducerPosts for case DELETE_POST to remove the post that is deleted from the application state.

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,13 @@
 import _ from 'lodash';
-import { FETCH_POSTS, FETCH_POST } from '../actions';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 
 export default function (state = {}, action) {
   switch (action.type) {
+    case DELETE_POST:
+      return _.omit(state, action.payload); // .omit use for object
+      // return _.reject(state, post === action.payload);
+      // if we have a state object of an array we would use .reject
+
     case FETCH_POST:
       // const post = action.payload.data;
       // const newState = { ...state,  };
